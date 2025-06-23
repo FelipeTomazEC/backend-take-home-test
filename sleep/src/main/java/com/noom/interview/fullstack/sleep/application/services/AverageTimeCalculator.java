dd9cb5af1ed95cfb5b8ba9f76426e10b87b99cf7
felipetomazec <felipetomaz2206@gmail.com>
2025-06-22 22:23:03 -0300

    feat: implement service to calculate average of times

---
+++ sleep/src/main/java/com/noom/interview/fullstack/sleep/application/services/AverageTimeCalculator.java
@@ -0,0 +1,15 @@
+package com.noom.interview.fullstack.sleep.application.services;
+
+import java.time.LocalTime;
+import java.util.List;
+
+public class AverageTimeCalculator {
+    public LocalTime calculateAvgTime(List<LocalTime> times) {
+        var averageInSeconds = times.stream()
+                .mapToLong(LocalTime::toSecondOfDay)
+                .average()
+                .orElse(0L);
+
+        return LocalTime.ofSecondOfDay((long) averageInSeconds);
+    }
+}
