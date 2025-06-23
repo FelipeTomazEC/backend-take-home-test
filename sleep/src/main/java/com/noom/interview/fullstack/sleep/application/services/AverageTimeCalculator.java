a6bb0131104d34af51ce4118ee3f636a4b14dbfe
felipetomazec <felipetomaz2206@gmail.com>
2025-06-22 23:10:53 -0300

    fix: add missing @Service annotation

--- sleep/src/main/java/com/noom/interview/fullstack/sleep/application/services/AverageTimeCalculator.java
+++ sleep/src/main/java/com/noom/interview/fullstack/sleep/application/services/AverageTimeCalculator.java
@@ -1,8 +1,11 @@
 package com.noom.interview.fullstack.sleep.application.services;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalTime;
 import java.util.List;
 
+@Service
 public class AverageTimeCalculator {
     public LocalTime calculateAvgTime(List<LocalTime> times) {
         var averageInSeconds = times.stream()
