ff090396f26cc12df49be67c5e6e2841f9b607b1
felipetomazec <felipetomaz2206@gmail.com>
2025-06-22 23:21:10 -0300

    feat: implements a repository to retrieve sleep logs from a specific period of time

---
+++ sleep/src/main/java/com/noom/interview/fullstack/sleep/infrastructure/database/mappers/SleepLogRowMapper.java
@@ -0,0 +1,27 @@
+package com.noom.interview.fullstack.sleep.infrastructure.database.mappers;
+
+import com.noom.interview.fullstack.sleep.domain.SleepLog;
+import com.noom.interview.fullstack.sleep.domain.SleepQuality;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class SleepLogRowMapper implements RowMapper<SleepLog> {
+
+    @Override
+    public SleepLog mapRow(ResultSet rs, int rowNum) throws SQLException {
+        var bedTime = rs.getObject("bed_time", LocalTime.class);
+        var wakeUpTime = rs.getObject("wake_up_time", LocalTime.class);
+        var sleepDate = rs.getObject("sleep_date", LocalDate.class);
+        var quality = SleepQuality.valueOf(rs.getString("quality"));
+
+        return SleepLog.builder()
+                .bedTime(sleepDate.minusDays(1).atTime(bedTime))
+                .wakeUpTime(sleepDate.atTime(wakeUpTime))
+                .quality(quality)
+                .build();
+    }
+}
