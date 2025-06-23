250a697ea0aedf42987bf7504e2f2ac7fe7e6716
felipetomazec <felipetomaz2206@gmail.com>
2025-06-23 12:10:34 -0300

    fix: change create sleep use case so users cannot create more than 1 sleep log per day

--- sleep/src/test/groovy/com/noom/interview/fullstack/sleep/application/usecases/CreateSleepLogUseCaseTest.groovy
+++ sleep/src/test/groovy/com/noom/interview/fullstack/sleep/application/usecases/CreateSleepLogUseCaseTest.groovy
@@ -1,20 +1,28 @@
 package com.noom.interview.fullstack.sleep.application.usecases
 
 import com.noom.interview.fullstack.sleep.application.ports.commands.CreateSleepLogCommand
+import com.noom.interview.fullstack.sleep.application.ports.repositories.GetSleepLogFromDateRepository
 import com.noom.interview.fullstack.sleep.application.ports.repositories.SaveSleepLogRepository
 import com.noom.interview.fullstack.sleep.domain.SleepLog
 import com.noom.interview.fullstack.sleep.domain.SleepQuality
+import com.noom.interview.fullstack.sleep.domain.errors.SleepLogAlreadyExistsException
+import com.noom.interview.fullstack.sleep.testutils.TestEntitiesBuilder
 import spock.lang.Specification
 import spock.lang.Subject
 
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 class CreateSleepLogUseCaseTest extends Specification {
 
     def sleepLogRepository = Mock(SaveSleepLogRepository)
+    def getLogFromSpecificDateRepository = Mock(GetSleepLogFromDateRepository)
 
     @Subject
-    def createSleepLogUseCase = new CreateSleepLogUseCase(sleepLogRepository)
+    def createSleepLogUseCase = new CreateSleepLogUseCase(
+            sleepLogRepository,
+            getLogFromSpecificDateRepository
+    )
 
     def "Create a sleep log and stores it in a repository"() {
         given: "A create sleep log command with valid parameters"
@@ -35,5 +43,26 @@ class CreateSleepLogUseCaseTest extends Specification {
             log.bedTime == command.bedTime.toLocalTime()
             log.wakeUpTime == command.wakeUpTime.toLocalTime()
         }, userId)
+    }
+
+    def "User cannot have more than one log per day"() {
+        given: "A user has an existing sleep log for today"
+        def userId = UUID.randomUUID()
+        getLogFromSpecificDateRepository.findByDate(LocalDate.now(), userId) >>
+                Optional.of(TestEntitiesBuilder.buildSleepLog().build())
+
+        and: "we try to create a new sleep log for today"
+        def command = CreateSleepLogCommand.builder()
+                .bedTime(LocalDateTime.now().minusHours(5))
+                .wakeUpTime(LocalDateTime.now())
+                .quality(SleepQuality.OK)
+                .userId(userId)
+                .build()
+
+        when: "The use case is executed"
+        createSleepLogUseCase.execute(command)
+
+        then: "An exception is thrown indicating the user already has a log for today"
+        thrown(SleepLogAlreadyExistsException)
     }
 }
